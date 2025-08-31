cd1a5405a8b547c59832ca4e11bfad06aac5f76b
حيدر الحاج احمد <haideralhajahmed2@gmail.com>
2025-08-31 19:06:44 +0300

    Add broadcasting configuration and test views for Pusher integration
    - Add MyPusherEvent for testing broadcasting
    - Configure broadcasting.php for Pusher
    - Add testPusher view for frontend testing
    - Exclude environment files for security

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/testPusher', function () {
+    return view('testPusher');
+});
+
+Route::get('/trigger-event', function () {
+    event(new \App\Events\MyPusherEvent('Hello from Laravel!'));
+    return 'Event triggered!';
+});
