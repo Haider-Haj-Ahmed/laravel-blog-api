9dc6bcbc2cf53fe09bdf2d668fa4163427fae9c6
Adonis <adonesshpep@gmail.com>
2026-08-19 18:05:45 +0300

    no drafts their

--- app/Http/Controllers/API/ProfileController.php
+++ app/Http/Controllers/API/ProfileController.php
@@ -231,18 +231,11 @@ class ProfileController extends Controller
         }
 
         $viewerId = auth('sanctum')->id();
-        if($viewerId==$user->id){
-            $postsQuery = $user->posts()
-            ->with('user')
-            ->with('tags')
-            ->latest();
-        }else{ 
         $postsQuery = $user->posts()
             ->with('user')
             ->with('tags')
             ->where('is_published', true)
             ->latest();
-        }
         if ($viewerId) {
             $postsQuery->withExists([
                 'views as is_viewed' => fn ($query) => $query->where('user_id', $viewerId),
@@ -274,18 +267,11 @@ class ProfileController extends Controller
         }
 
         $viewerId = auth('sanctum')->id();
-        if($viewerId==$user->id){
-                    $blogsQuery = $user->blogs()
-            ->with('user')
-            ->with('tags')
-            ->latest();
-        }else{
         $blogsQuery = $user->blogs()
             ->with('user')
             ->with('tags')
             ->where('is_published', true)
             ->latest();
-        }
         if ($viewerId) {
             $blogsQuery->withExists([
                 'views as is_viewed' => fn ($query) => $query->where('user_id', $viewerId),
